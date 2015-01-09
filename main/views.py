fd9793698d0cb8fb01c952c9d5957b77a73127a9
William Morrell <wcmorrell@lbl.gov>
2015-01-08 16:53:12 -0800

    Not using function definitions for views anymore.

--- main/views.py
+++ main/views.py
@@ -11,9 +11,6 @@ class IndexView(generic.ListView):
     model = Study
     template_name = 'main/index.html'
     context_object_name = 'study_list'
-    
-    #def get_queryset(self):
-    #    return Study.objects.all()
 
 
 class StudyDetailView(generic.DetailView):
@@ -22,9 +19,3 @@ class StudyDetailView(generic.DetailView):
     model = Study
     template_name = 'main/detail.html'
 
-
-def index(request):
-    return render(request, 'main/index.html', { 'study_list': Study.objects.all() })
-
-def detail(request, study_id):
-    return render(request, 'main/detail.html', { 'study': get_object_or_404(Study, pk=study_id) })
