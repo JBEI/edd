8cb7e69e7e2a7c45301f81fe78c7d4527a3586b6
William Morrell <wcmorrell@lbl.gov>
2015-07-21 13:06:49 -0700

    Using + related_name on Update ForeignKey objects was causing unnecessary joins against MeasurementValue on some queries.

--- main/models.py
+++ main/models.py
@@ -113,7 +113,7 @@ class Comment(models.Model):
         db_table = 'comment'
     object_ref = models.ForeignKey('EDDObject', related_name='comments')
     body = models.TextField()
-    created = models.ForeignKey(Update, related_name='+')
+    created = models.ForeignKey(Update)
 
     def save(self, *args, **kwargs):
         if self.created_id is None:
@@ -129,7 +129,7 @@ class Attachment(models.Model):
     object_ref = models.ForeignKey('EDDObject', related_name='files')
     file = models.FileField(max_length=255)
     filename = models.CharField(max_length=255)
-    created = models.ForeignKey(Update, related_name='+')
+    created = models.ForeignKey(Update)
     description = models.TextField(blank=True, null=False)
     mime_type = models.CharField(max_length=255, blank=True, null=True)
     file_size = models.IntegerField(default=0)
@@ -281,8 +281,8 @@ class EDDObject(models.Model):
     active = models.BooleanField(default=True)
     updates = models.ManyToManyField(Update, db_table='edd_object_update', related_name='+')
     # these are used often enough we should save extra queries by including as fields
-    created = models.ForeignKey(Update, related_name='+', editable=False)
-    updated = models.ForeignKey(Update, related_name='+', editable=False)
+    created = models.ForeignKey(Update, related_name='object_created', editable=False)
+    updated = models.ForeignKey(Update, related_name='object_updated', editable=False)
     # store arbitrary metadata as a dict with hstore extension
     meta_store = HStoreField(blank=True, default=dict)
 
@@ -1108,7 +1108,7 @@ class Measurement(models.Model):
     measurement_type = models.ForeignKey(MeasurementType)
     x_units = models.ForeignKey(MeasurementUnit, related_name='+')
     y_units = models.ForeignKey(MeasurementUnit, related_name='+')
-    update_ref = models.ForeignKey(Update, related_name='+')
+    update_ref = models.ForeignKey(Update)
     active = models.BooleanField(default=True)
     compartment = models.CharField(max_length=1,
                                    choices=MeasurementCompartment.GROUP_CHOICE,
@@ -1210,7 +1210,7 @@ class MeasurementValue(models.Model):
     measurement = models.ForeignKey(Measurement)
     x = ArrayField(models.DecimalField(max_digits=16, decimal_places=5))
     y = ArrayField(models.DecimalField(max_digits=16, decimal_places=5))
-    updated = models.ForeignKey(Update, related_name='+')
+    updated = models.ForeignKey(Update)
 
     def __str__(self):
         return '(%s, %s)' % (self.x, self.y)
