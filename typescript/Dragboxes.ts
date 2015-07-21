3db31f9eb0b668c9fc99c405bb8fb9f4133a990a
William Morrell <wcmorrell@lbl.gov>
2015-07-21 14:05:25 -0700

    Improvements to Dragboxes code for drag-selecting checkboxes in a table.

--- typescript/Dragboxes.ts
+++ typescript/Dragboxes.ts
@@ -17,22 +17,31 @@ module Dragboxes {
 
     export function dragOver(event) {
         if (globalChecked !== null) {
-            $(':checkbox', this).prop('checked', globalChecked);
+            $(':checkbox', this).prop('checked', globalChecked).trigger('change');
         }
     }
 
     export function dragStart(event) {
+        var $this = $(this), table;
         // mousedown toggles the clicked checkbox value and stores new value in globalChecked
+        if (globalChecked === null) {
+            // have to check for null to prevent double event from clicking label
+            $this.prop('checked', (i, value) => { return (globalChecked = !value); });
+        }
         // also attaches mouseover event to all cells in parent table
-        var table = $(this).prop('checked', (i, value) => { return (globalChecked = !value); })
-            .closest('.dragboxes').on('mouseover.dragboxes', 'td', dragOver);
+        table = $(this).closest('.dragboxes').on('mouseover.dragboxes', 'td', dragOver);
         // wait for mouse to go up anywhere, then end drag events
         $(document).one('mouseup.dragboxes', { 'table': table }, dragEnd);
         return false;
     }
 
     export function initTable(table) {
-        $(table).filter('.dragboxes').on('mousedown.dragboxes', 'td :checkbox', dragStart)
-            .on('mousedown.dragboxes', 'td label', dragStart);
+        $(table).filter('.dragboxes')
+            // watch for mousedown on checkboxes
+            .on('mousedown.dragboxes', 'td :checkbox', dragStart)
+            // also watch for mousedown on labels
+            .on('mousedown.dragboxes', 'td label', dragStart)
+            // disable click because mousedown is handling it now
+            .on('click.dragboxes', 'td :checkbox', () => false);
     }
 }
