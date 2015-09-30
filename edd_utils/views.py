739ab0a060fe98d4f3104c8e78c62d66e7a66d8a
William Morrell <wcmorrell@lbl.gov>
2015-09-29 17:14:33 -0700

    Cleaning up views.py

--- edd_utils/views.py
+++ edd_utils/views.py
@@ -3,8 +3,8 @@
 
 import json
 import logging
-import re
 import os
+import re
 
 from django.contrib import messages
 from django.core.urlresolvers import reverse
@@ -14,20 +14,18 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 from functools import partial
 from io import BytesIO
 
-from . import cytometry, gc_ms_workbench
+from . import cytometry, data_excelreader, data_map_ecdata, gc_ms_workbench
+from .net_smb import SMBController
 from .parsers import excel, skyline
 from main.forms import CreateStudyForm
-from .net_smb import SMBController
-
-import data_excelreader
-import data_map_ecdata
 
 
 logger = logging.getLogger(__name__)
 
 
-def utilities_index (request) :
+def utilities_index(request):
     return render(request, 'index.html', {})
+
 
 ########################################################################
 # GC-MS
@@ -36,6 +34,7 @@ def utilities_index (request) :
 def gcms_home(request):
     """Starting point for extracting peaks from ChemStation report files."""
     return render(request, 'gc_ms.html', {})
+
 
 def gcms_parse(request):
     """ Process an Agilent MSDChemStation report and return a table of data as JSON string. """
@@ -46,14 +45,16 @@ def gcms_parse(request):
         assert isinstance(json_result, dict)
         return JsonResponse(json_result)
     except (AttributeError, KeyError, ValueError) as e:
-        return JsonResponse({ 'python_error' : str(e) })
+        return JsonResponse({'python_error': '%s' % e, })
+
 
 def gcms_merge(request):
     data = json.loads(request.body)
     try:
         return JsonResponse(gc_ms_workbench.finalize_gc_ms_spreadsheet(data))
     except RuntimeError as e:
-        return JsonResponse({ 'python_error' : str(e) })
+        return JsonResponse({'python_error': '%s' % e, })
+
 
 def gcms_export(request):
     form = request.POST
@@ -66,14 +67,14 @@ def gcms_export(request):
         # options, otherwise this won't work
         f = BytesIO(gc_ms_workbench.export_to_xlsx(table, headers))
         file_name = prefix + ".xlsx"
-        response = HttpResponse(f,
-            content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+        response = HttpResponse(
+            f, content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
         response['Content-Disposition'] = 'attachment; filename="%s"' % file_name
         return response
     except Exception as e:
         messages.error(request, "Could not generate the GC-MS export file: %s" % e)
         return HttpResponse(status=500)
-#
+
 
 ########################################################################
 # PROTEOMICS
@@ -82,6 +83,7 @@ def gcms_export(request):
 def skyline_home(request):
     return render(request, 'skyline_import.html', {})
 
+
 def skyline_parse(request):
     try:
         data = request.FILES['file'].read()
@@ -89,7 +91,7 @@ def skyline_parse(request):
         assert (result is not None)
         return JsonResponse(result.export())
     except Exception as e:
-        return JsonResponse({ 'python_error': str(e) })
+        return JsonResponse({'python_error': '%s' % e, })
 
 
 ########################################################################
@@ -100,6 +102,7 @@ def cytometry_home(request):
     return render(request, 'cytometry.html', {
         'study_form': study_form,
         })
+
 
 def cytometry_parse(request):
     upload = request.FILES.get('file', None)
@@ -114,16 +117,17 @@ def cytometry_parse(request):
             for sheet in parsed.get('worksheets', []):
                 for ws in sheet:
                     header = ws.get('headers', [])
-                    table = [ u'\t'.join(map(replace, map(unicode, header))) ] if header else []
+                    table = [u'\t'.join(map(replace, map(unicode, header))) ] if header else []
                     for row in ws.get('values', []):
                         table.append(u'\t'.join(map(replace, map(unicode, row))))
                     tables.append(u'\n'.join(table))
-            return JsonResponse({ 'data': u'\n\n'.join(tables) })
+            return JsonResponse({'data': u'\n\n'.join(tables), })
         else:
             # try to parse as plain text
-            return JsonResponse({ 'data': upload.read() })
+            return JsonResponse({'data': upload.read(), })
     except Exception as e:
-        return JsonResponse({ 'python_error': str(e) })
+        return JsonResponse({'python_error': '%s' % e, })
+
 
 def cytometry_import(request):
     if (request.method != "POST"):
@@ -149,22 +153,25 @@ def cytometry_import(request):
 
 MAX_PATH_LENGTH = 260
 
+
 def ec_import(request):
     return render(request, 'ec_import.html', {})
 
+
 def ec_import_action(request, target, path):
-    data = {'result':'success'}
+    data = {'result': 'success'}
     if len(target) > MAX_PATH_LENGTH:
         data['result'] = 'error'
         data['message'] = 'Path length max size exceeded.'
     # TODO: sanatize to prevent injections!
-    
+
     else:
-        #TODO: Check if samba drive is mounted first
+        # TODO: Check if samba drive is mounted first
 
         # remote_folder_path = "/SPMAX-M2-02/Jason/"
-        # remote_file_path = "/SPMAX-M2-02/ktran/HTP-Pre/cellulase dispensing/cell_disp_60uL_4_25_13_kt.xlsx"
-        
+        # remote_file_path = "/SPMAX-M2-02/ktran/HTP-Pre/cellulase dispensing/"
+        #                    "cell_disp_60uL_4_25_13_kt.xlsx"
+
         # TODO: local file path must be non-colliding
         volume = "instrumentdata"
         remote_file_path = path
@@ -173,10 +180,10 @@ def ec_import_action(request, target, path):
         try:
             os.mkdir('output')
         except OSError:
-            pass # already exists
-        #TODO: IOError
-        
-        #TODO try: except:
+            pass  # already exists
+        # TODO: IOError
+
+        # TODO try: except:
         smb = SMBController()
         smb.retrieve_file(volume, remote_file_path, local_file_path)
 
@@ -192,6 +199,3 @@ def ec_import_action(request, target, path):
 
     # TODO: kickback any EDD-specific exceptions
     return JsonResponse(data)
-
-
-
