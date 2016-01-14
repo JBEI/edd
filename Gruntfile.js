df2eee92ffd76d79a1a5ab537348451b4b592e95
Garrett Birkel <GWBirkel@lbl.gov>
2016-01-13 19:17:21 -0800

    New gruntfile.  Invoke with "--production" to build production code.  Invoke with "--watch" to build dev code and watch the src folder.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,74 @@
 module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-typescript');
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
 	grunt.initConfig({
 	    typescript: {
-	      base: {
-	        src: ['./typescript/*.ts'],
-	        dest: './main/static/main/js/',
+	      buildDev: {
+	        src: ['./typescript/build/*.ts'],
+	        dest: './typescript/build/',
 	        options: {
-			  rootDir: './typescript',
+			  rootDir: './typescript/build/',
+			  target: 'es5',
+			  declaration: false,
 	          sourceMap: true,
-              declaration: true,
-	          removeComments: false 
+   	          removeComments: false,
+	        }
+	      },
+	      buildProd: {
+	        src: ['./typescript/build/*.ts'],
+	        dest: './typescript/build/',
+	        options: {
+			  rootDir: './typescript/build/',
+			  target: 'es5',
+			  declaration: false,
+	          sourceMap: false,
+   	          removeComments: true,
 	        }
 	      }
-	    }
+	    },
+		copy: {
+		  prep: {
+		    cwd: './typescript/src/',      // set working folder / root to copy
+		    src: '**/*',                               // copy all files and subfolders
+		    dest: './typescript/build/', // destination folder
+		    expand: true                               // required when using cwd
+		  },
+		  merge: {
+		    cwd: './typescript/build/',
+		    src: '**/*',
+		    dest: './static/main/js/',
+		    expand: true
+		  }
+		},
+		watch: {
+		    scripts: {
+		        files: ['./typescript/src/*.ts'],                 // the watched files
+		        tasks: ["copy:prep", "typescript:buildDev", "copy:merge"],  // the task(s) to run
+		        options: {
+		            spawn: false // makes the watch task faster
+		        }
+		    }
+		}
 	});
 
-  	grunt.registerTask('default', 'typescript');
 
+    var production = grunt.option('production');
+    var watch = grunt.option('watch');
+
+    if (production) {
+    	// One-time production build
+	  	grunt.registerTask('default', ["copy:prep", "typescript:buildProd", "copy:merge"]);
+    } else if (watch) {
+    	// Dev build and watch for changes
+	  	grunt.registerTask('default', ["copy:prep", "typescript:buildDev", "copy:merge", 'watch']);
+    } else {
+    	// Standard one-time dev build
+	  	grunt.registerTask('default', ["copy:prep", "typescript:buildDev", "copy:merge"]);
+    }
 };
+
 
 
