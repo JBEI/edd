a5e6fdad3bdd50c10370e3238179a745d803a278
Mark Forrer <mark.forrer@lbl.gov>
2016-02-24 09:42:30 -0800

    Reset previous changes to edd/__init__.py in an attempt to fix errors in test deployment. If memory serves, these changes were needed during initial REST work that used Django ORM model references on the client side, but shouldn't be required anymore.

--- edd/__init__.py
+++ edd/__init__.py
@@ -1,18 +1,13 @@
 # coding: utf-8
 from __future__ import absolute_import, unicode_literals
 
-import importlib
 import logging
 import os
 
 # specify a default settings, in case DJANGO_SETTINGS_MODULE env is not set
-settings_module = os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edd.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edd.settings")
 
-# dynamically import the settings module configured via environment variable OR specific command
-# within the host script. Note that we can't use django.conf.settings here, since we're only
-# importing custom EDD settings that unfortunately aren't persisted by django.conf.settings as are
-#  the django-defined settings (although we define both in the same file).
-custom_settings = importlib.import_module(settings_module)
+from django.conf import settings
 
 # patch the default formatter to use a unicode format string
 logging._defaultFormatter = logging.Formatter("%(message)s")
@@ -20,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 # If configured, make sure the celery app is imported when
 # Django starts so that @shared_task will use this app.
-if custom_settings.USE_CELERY:
+if settings.USE_CELERY:
     logger.info("Using Celery distributed task queue")
     from .celery import task_exchange as celery_app  # noqa
 else:
