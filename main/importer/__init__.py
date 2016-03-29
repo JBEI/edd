40217f939206e7803b0a43747c9ccdfe7d6650f4
William Morrell <wcmorrell@lbl.gov>
2016-03-29 16:53:15 -0700

    Changing local.py-example to be more explicit about logging

--- main/importer/__init__.py
+++ main/importer/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
 from .table import TableImport  # noqa
-from .rnaseq import (
+from .rnaseq import (  # noqa
     import_rna_seq, import_rnaseq_edgepro, interpret_edgepro_data, interpret_raw_rna_seq_data
-)  # noqa
+)
