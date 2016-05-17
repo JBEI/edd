42ee11ae410d97eedcc0e70ad7d5070a35cb21a1
Traci Lopez <tlopez@tlopez-mr.dhcp.lbl.gov>
2016-05-17 15:09:25 -0700

    user can successfully login to EDD even if Solr server is not running

--- main/signals/handlers.py
+++ main/signals/handlers.py
@@ -11,12 +11,15 @@ from django.core.urlresolvers import reverse as urlreverse
 from django.db import connection, transaction
 from django.db.models.signals import m2m_changed, post_delete, post_save, pre_save, pre_delete
 from django.dispatch import receiver
+from django.core.mail import send_mail
 
 from jbei.ice.rest.ice import parse_entry_id, HmacAuth
 from . import study_modified, study_removed, user_modified
 from ..models import Line, Strain, Study, Update
 from ..solr import StudySearch, UserSearch
 from ..utilities import get_absolute_url
+
+
 
 solr = StudySearch()
 users = UserSearch()
@@ -57,7 +60,14 @@ def unindex_study(sender, study, **kwargs):
 
 @receiver(user_modified)
 def index_user(sender, user, **kwargs):
-    users.update([user, ])
+    def email_admin():
+        send_mail('Solr Server is down', 'Please fix.', 'support@lbl.gov', ['tlopez@lbl.gov'], fail_silently=False)
+
+    try:
+        users.update([user, ])
+    except Exception as e:
+        email_admin()
+        logging.exception("Solr server needs to be restarted", e)
 
 
 def log_update_warning_msg(study_id):
