b2501412e0e4f5ffd08e5ee28d1744e102dd1153
William Morrell <wcmorrell@lbl.gov>
2016-07-11 13:54:20 -0700

    Typos in init-config.sh while generating secrets; replace local.py admin email with the current user git name/email

--- init-config.sh
+++ init-config.sh
@@ -8,16 +8,23 @@ if [ ! -f "$DIR/secrets.env" ]; then
     # replacing the secret{n} values
     while [ $COUNTER -lt 5 ]; do
         EDD_SECRET=`echo "secret${COUNTER} $(date)" | shasum | cut -c 1-32`
-        sed -i -e "s/secret${COUNTER}/${EDD_SECRET}" "$DIR/secrets.env"
-        echo "secret${COUNTER} = ${EDD_SECRET}"
+        sed -i -e "s/secret${COUNTER}/${EDD_SECRET}/" "$DIR/secrets.env"
         let COUNTER=COUNTER+1
     done
     # replace Django secret
     EDD_SECRET=`echo "secret${COUNTER} $(date)" | shasum | cut -c 1-32`
-    set -i -e "s/put some random secret text here/${EDD_SECRET}" "$DIR/secrets.env"
+    set -i -e "s/put some random secret text here/${EDD_SECRET}/" "$DIR/secrets.env"
 fi
 
 if [ ! -f "$DIR/edd/settings/local.py" ]; then
     echo "Copying example local.py settings …"
     cp "$DIR/edd/settings/local.py-example" "$DIR/edd/settings/local.py"
+    EDD_USER=$(git config --get user.name)
+    EDD_EMAIL=$(git config --get user.email)
+    if [ ! -z "$EDD_USER" ] && [ ! -z "$EDD_EMAIL" ]; then
+        sed -i -e "s/'Jay Bay'/'${EDD_USER}'/;s/'admin@example.org'/'${EDD_EMAIL}'/" \
+            "$DIR/edd/settings/local.py"
+    else
+        echo "Warning! Have not replaced admin email in $DIR/edd/settings/local.py"
+    fi
 fi
