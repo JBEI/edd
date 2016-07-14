0e430bda953a4408e1c621a325a3b19b48184abc
Mark Forrer <mark.forrer@lbl.gov>
2016-07-14 15:28:53 -0700

    Updated README.md and appserver_entrypoint.sh based on a read-through of the shell script for another purpose -- looks like the dump file doesn't drop / re-create the database by default, so most of the fast-scrolling output was probably undetected error messages. Also added an EDD_DEPLOYMENT_ENVIRONMENT variable to simplify running code in development where code changes are frequent, plus some extra shell output to help interpret what's going on without reading the script.

--- docker_services/appserver_entrypoint.sh
+++ docker_services/appserver_entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -o pipefail -e
 
+SEPARATOR='****************************************************************************************'
+echo "EDD_DEPLOYMENT_ENVIRONMENT: " ${EDD_DEPLOYMENT_ENVIRONMENT:-'Not specified. Assuming PRODUCTION.'}
+
 # TODO: check for $EDD_HOST_DIR; if found, proceed
 #   if $EDD_HOST_DIR not found, test for contents of /code
 #   if /code does not contain EDD code, run git clone
@@ -12,9 +15,17 @@ until nc -z redis 6379; do
     sleep 1
 done
 
+echo
+echo "$SEPARATOR"
+echo "Collecting static resources..."
+echo "$SEPARATOR"
 # Collect static first, worker will complain if favicons are missing
 python /code/manage.py collectstatic --noinput
 
+echo
+echo "$SEPARATOR"
+echo "Configuring database and Solr indexes..."
+echo "$SEPARATOR"
 # Wait for postgres to become available
 until nc -z postgres 5432; do
     echo "Waiting for postgres server …"
@@ -24,18 +35,29 @@ done
 # Test if our database exists; run init script if missing
 export PGPASSWORD=$POSTGRES_PASSWORD
 if ! psql -lqt -h postgres -U postgres | cut -d \| -f 1 | grep -qw edd; then
+    echo "$SEPARATOR"
     echo "Initializing the database for first-time use …"
     psql -h postgres -U postgres template1 < /code/docker_services/postgres/init.sql
 fi
-# If database dump URL is provided, dump and restore database
+
+# if new data is provided, drop and re-create the existing database to prepare for receiving the
+# dump
+if [ ! -z $POSTGRES_DUMP_URL ] || ([ ! -z $POSTGRES_DUMP_FILE ] && [ -r $POSTGRES_DUMP_FILE ]); then
+    echo "Dropping existing database edd"
+    psql -h postgres -U postgres -c 'DROP DATABASE IF EXISTS edd;'
+    psql -h postgres -U postgres -c 'CREATE DATABASE edd;'
+fi
+
+# If database dump URL is provided, dump the reference database and restore the local one from
+# the dump
 if [ ! -z $POSTGRES_DUMP_URL ]; then
     echo "Copying database from remote $POSTGRES_DUMP_URL …"
     pg_dump "$POSTGRES_DUMP_URL" | psql -h postgres -U postgres edd
 elif [ ! -z $POSTGRES_DUMP_FILE ] && [ -r $POSTGRES_DUMP_FILE ]; then
     echo "Copying database from local file $POSTGRES_DUMP_FILE …"
-    psql -h postgres -U posgtres edd < "$POSTGRES_DUMP_FILE"
+    psql -h postgres -U postgres edd < "$POSTGRES_DUMP_FILE"
 else
-    echo "Skipping database restore …"
+    echo "Skipping database restore"
 fi
 unset PGPASSWORD
 unset POSTGRES_DUMP_FILE
@@ -47,12 +69,31 @@ until nc -z solr 8983; do
     sleep 1
 done
 
-# Run migrations
+echo
+echo "$SEPARATOR"
+echo "Running database migrations..."
+echo "$SEPARATOR"
 python /code/manage.py migrate
 
-# Re-index
-# TODO: be smarter about when this needs to run
-python /code/manage.py edd_index
+
+if [ ! -z $POSTGRES_DUMP_URL ] || ([ ! -z $POSTGRES_DUMP_FILE ] && [ -r $POSTGRES_DUMP_FILE ]); then
+    echo "$SEPARATOR"
+    echo "Re-building Solr indexes..."
+    python /code/manage.py edd_index
+fi
 
 # Start up the application server
-gunicorn -w 4 -b 0.0.0.0:8000 edd.wsgi:application
+echo
+echo "$SEPARATOR"
+if [ "$EDD_DEPLOYMENT_ENVIRONMENT" ==  "DEVELOPMENT" ]; then
+    echo "Starting development apppserver"
+    echo "$SEPARATOR"
+    python manage.py runserver 0.0.0.0:8000
+else
+    echo "Starting production appserver"
+    echo "$SEPARATOR"
+    gunicorn -w 4 -b 0.0.0.0:8000 edd.wsgi:application
+fi
+
+unset EDD_DEPLOYMENT_ENVIRONMENT
+
