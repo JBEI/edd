5f61903d91a40cea6cba262ad4d2dd50869fe97a
William Morrell <wcmorrell@lbl.gov>
2016-07-25 13:18:47 -0700

    Capture environment from EDD-280 in Django settings, add custom middleware attaching the deployment environment to the request for use in templates.

--- edd/settings/base.py
+++ edd/settings/base.py
@@ -74,6 +74,7 @@ SECRET_KEY = env('SECRET_KEY', default='I was awake and dreaming at the same tim
 
 ALLOWED_HOSTS = []
 SITE_ID = 1
+EDD_DEPLOYMENT_ENVIRONMENT = env('EDD_DEPLOYMENT_ENVIRONMENT', 'PRODUCTION')
 LOGIN_REDIRECT_URL = '/'
 
 # Application definition
@@ -112,6 +113,7 @@ MIDDLEWARE_CLASSES = (
     'threadlocals.middleware.ThreadLocalMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'main.utilities.EDDSettingsMiddleware',
 )
 
 
