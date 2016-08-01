f037519649fc32496b6081dab0d5918cabf6717b
Traci Lopez <tlopez@lbl.gov>
2016-08-01 16:20:41 -0700

    grunt test runs once without auto watching

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,7 +86,8 @@ module.exports = function(grunt) {
         },
         karma: {
           unit: {
-            configFile: 'karma.conf.js'
+            configFile: 'karma.conf.js',
+            singleRun: true
           }
         },
         concat: {
