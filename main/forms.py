4b92010c53e168c54768f32c4a1658a3d059d46d
Traci Lopez <tlopez@lbl.gov>
2016-08-09 17:54:18 -0700

    added this field is required under line label

--- main/forms.py
+++ main/forms.py
@@ -481,7 +481,8 @@ class LineForm(forms.ModelForm):
         # store the parent Study
         self._study = kwargs.pop('study', None)
         super(LineForm, self).__init__(*args, **kwargs)
-        self.fields['name'].error_messages= {'required': 'Line name is required'}
+        self.fields['name'].help_text = 'This field is required.'
+        # self.labels['name'].error_messages= {'required': 'Line name is required'}
         # alter all fields to include a "bulk-edit" checkbox in label
         # initially hidden via "off" class
         for fieldname, field in self.fields.items():
