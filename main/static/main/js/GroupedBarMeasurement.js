c94386c922e28dcc936ca03419b00d8d651f8741
Traci Lopez <tlopez@lbl.gov>
2016-08-15 15:51:29 -0700

    multi axis for grouped bar charts

--- main/static/main/js/GroupedBarMeasurement.js
+++ main/static/main/js/GroupedBarMeasurement.js
@@ -4,16 +4,20 @@
  **/
  function createMeasurementGraph(graphSet, svg, type) {
 
-    var assayMeasurements = graphSet.assayMeasurements;
+    var assayMeasurements = graphSet.assayMeasurements,
+        numUnits = howManyUnits(assayMeasurements),
+        yRange = [],
+        unitMeasurementData = [],
+        yMin = [];
 
-    //x axis scale for assay's protein name
+    //x axis scale for type
     var x_name = d3.scale.ordinal()
         .rangeRoundBands([0, graphSet.width], 0.1);
     
     //x axis scale for x values
     var x_xValue = d3.scale.ordinal();
     
-    //x axis scale for line id to differentiate multiple lines associated with the same protein
+    //x axis scale for line id to differentiate multiple lines associated with the same name/type
     var lineID = d3.scale.ordinal();
 
     // y axis range scale
@@ -23,27 +27,48 @@
     var div = d3.select("body").append("div")
         .attr("class", "tooltip2")
         .style("opacity", 0);
-
-    //nest data by name. nest again by x label
-    var nested = d3.nest(type)
-        .key(function (d) { 
-            return d[type];
-        })
+    
+    var meas = d3.nest()
         .key(function (d) {
-            return parseFloat(d.x);
+            return d.y_unit;
         })
         .entries(assayMeasurements);
 
-    data = getXYValues(nested);
+    for (var i = 0; i < numUnits; i++) {
+        yRange.push(d3.scale.linear().rangeRound([graphSet.height, 0]));
+        unitMeasurementData.push(d3.nest()
+            .key(function (d) {
+                return d.y;
+            })
+            .entries(meas[i].values));
+        yMin.push(d3.min(unitMeasurementData[i], function (d) {
+        return d3.min(d.values, function (d) {
+            return d.y;
+        });
+    }))
+    }
 
-    var proteinNames = data.map(function (d) {
-        return d.key;
-    });
+    var nested = d3.nest(type)
+            .key(function (d) {
+                return d[type];
+            })
+            .key(function (d) {
+                return parseFloat(d.x);
+            })
+            .entries(assayMeasurements);
 
-    //sort x values
-    proteinNames.sort(function(a, b) { return a - b});
+    var data = getXYValues(nested);
 
-    var data2 = data.map(function (d) {
+    var typeNames = data.map(function (d) {
+            return d.key;
+        });
+
+        //sort x values
+        typeNames.sort(function (a, b) {
+            return a - b
+        });
+
+    var xValues = data.map(function (d) {
         return (d.values);
     });
 
@@ -53,140 +78,202 @@
     });
 
     // returns x values
-    var xValueLabels = data2[0].map(function (d) {
+    var xValueLabels = xValues[0].map(function (d) {
         return (d.key);
     });
 
     var sortedXvalues = xValueLabels.sort(function(a, b) { return parseFloat(a) - parseFloat(b)});
 
-    ymin = d3.min(assayMeasurements, function (d) {
-        return d.y;
-    });
-    
-    if (ymin >= 0) {
-      ymin = 0;
-    }
-
-    var wordLength = getSum(proteinNames);
+    var wordLength = getSum(typeNames);
 
     if (wordLength > 90) {
-       proteinNames = _.range(proteinNames.length);
+       typeNames = _.range(typeNames.length);
     }
 
-    x_name.domain(proteinNames);
+    x_name.domain(typeNames);
 
     x_xValue.domain(sortedXvalues).rangeRoundBands([0, x_name.rangeBand()]);
 
     lineID.domain(yvalueIds).rangeRoundBands([0, x_xValue.rangeBand()]);
-    y.domain([ymin, d3.max(assayMeasurements, function (d) {
-        return d.y;
-    })]);
-
-    //create x and y axis 
+    
+    //create x axis
     graphSet.create_x_axis(graphSet, x_name, svg);
-    graphSet.create_y_axis(graphSet, y, svg);
+    
+    for (var index = 0; index<numUnits; index++) {
 
-    var labelLength = data.length;
+        //y axis min and max domain 
+        y.domain([yMin[index], d3.max(unitMeasurementData[index], function (d) {
+            return d3.max(d.values, function (d) {
+                return d.y;
+            });
+        })]);
 
-    var names_g = svg.selectAll(".group")
-        .data(data)
-        .enter().append("g")
-        .attr("class", function (d) {
-            return 'group group-' + d.key;  //returns assay names
-        })
-        .attr("transform", function (d) {
-            return "translate(" + x_name(d.key) + ",0)";
-        });
+        nested = d3.nest(type)
+            .key(function (d) {
+                return d[type];
+            })
+            .key(function (d) {
+                return parseFloat(d.x);
+            })
+            .entries(meas[index].values);
 
-    var categories_g = names_g.selectAll(".category")
-        .data(function (d) {
-            return d.values;
-        })
-        .enter().append("g")
-        .attr("class", function (d) {
-            return 'category category-' + d.key;   // returns objects with key = value
-        })
-        .attr("transform", function (d) {
-            return "translate(" + x_xValue(d.key) + ",0)";
-        });
-
-    var categories_labels = categories_g.selectAll('.category-label')
-        .data(function (d) {
-            return [d.key];
-        })
-        .enter()
-        .append("text")
-        .attr("class", function (d) {
-            return 'category-label category-label-' + d;
-        })
-        .attr("x", function (d) {
-            return x_xValue.rangeBand() / 2;
-        })
-        .attr('y', function (d) {
-            return graphSet.height + 27;
-        })
-        .attr('text-anchor', 'middle')
-        .text(function (d) {
-            if (type == 'x') {
-                return
-            } if (labelLength > 4 && xValueLabels.length > 15) {
-                return
-            } if (labelLength > 12 && xValueLabels.length > 6) {
-                return
-            } else {
-                return d;
-            }
-        })
-        .style("font-size", 8);
+        data = getXYValues(nested);
 
 
-    var values_g = categories_g.selectAll(".value")
-        .data(function (d) {
-            return d.values;
-        })
-        .enter().append("g")
-        .attr("class", function (d) {
-            return 'value value-' + d.i;
-        })
-        .attr("transform", function (d) {
-            return "translate(" + lineID(d.key) + ",0)";
-        });
+        if (index == 0) {
+            var yAxis = d3.svg.axis().scale(y)
+                .orient("left").ticks(5);
 
-    var rects = values_g.selectAll('.rect')
-        .data(function (d) {
-            return [d];
-        })
-        .enter().append("rect")
-        .attr("class", "rect")
-        .attr("width", lineID.rangeBand())
-        .attr("y", function (d) {
-            return y(d.y);
-        })
-        .attr("height", function (d) {
-            return graphSet.height - y(d.y);
-        })
-        .style("fill", function (d) {
-            return graphSet.color(d.key);
-        })
-        .style("opacity", 0.3);
+            svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", -47)
+                .attr("x", 0 - (graphSet.height / 2))
+                .attr("dy", "1em")
+                .style("text-anchor", "middle")
+                .text(meas[index].key);
+            // Draw the y Grid lines
+            svg.append("g")
+                .attr("class", "grid")
+                .call(yAxis
+                    .tickSize(-graphSet.width, 0, 0)
+                    .tickFormat(""));
 
-    var hover = categories_g.selectAll('.rect')
-        .data(function (d) {
-            return d.values;
-        })
-        .on("mouseover", function (d) {
-            div.transition()
-                .style("opacity", 0.9);
-            div.html("<strong>" + d.name + "</strong></br>" + d.y + ": " + d.y_unit + "</br>" + d.x
-                     + ":  " + d.x_unit + "</br>" + "measurement: " + d.measurement)
-                .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
-        })
-        .on("mouseout", function () {
-            div.transition()
-                .style("opacity", 0);
-        });
+        } else {
+            var spacing = {
+                1: graphSet.width,
+                2: graphSet.width + 50,
+                3: graphSet.width + 100,
+                4: graphSet.width + 150
+            };
 
+            var yAxis = d3.svg.axis().scale(y)
+                .orient("right").ticks(5);
+
+            svg.append("g")
+                .attr("class", "y axis")
+                .attr("transform", "translate(" + spacing[index] + " ,0)")
+                .style("fill", "black")
+                .call(yAxis)
+                .append('text')
+                .attr("transform", "rotate(-90)")
+                .attr('x', -110)
+                .attr("dy", ".32em")
+                .attr('y', 35)
+                .style('text-anchor', 'middle')
+                .text(meas[index].key)
+        }
+
+        var labelLength = data.length;
+        var names_g = names_g +  index,
+            categories_g = categories_g  + index,
+            categories_labels = categories_labels  + index,
+            values_g = values_g  + index,
+            rects = rects  + index,
+
+        names_g = svg.selectAll(".group" + index)
+            .data(data)
+            .enter().append("g")
+            .attr("class", function (d) {
+                return 'group group-' + d.key;  //returns assay names
+            })
+            .attr("transform", function (d) {
+                return "translate(" + x_name(d.key) + ",0)";
+            });
+
+        categories_g = names_g.selectAll(".category" + index)
+            .data(function (d) {
+                return d.values;
+            })
+            .enter().append("g")
+            .attr("class", function (d) {
+                return 'category category-' + d.key;   // returns objects with key = value
+            })
+            .attr("transform", function (d) {
+                return "translate(" + x_xValue(d.key) + ",0)";
+            });
+
+        categories_labels = categories_g.selectAll('.category-label' + index)
+            .data(function (d) {
+                return [d.key];
+            })
+            .enter()
+            .append("text")
+            .attr("class", function (d) {
+                return 'category-label category-label-' + d;
+            })
+            .attr("x", function () {
+                return x_xValue.rangeBand() / 2;
+            })
+            .attr('y', function () {
+                return graphSet.height + 27;
+            })
+            .attr('text-anchor', 'middle')
+            .text(function (d) {
+                if (type == 'x') {
+                    return
+                }
+                if (labelLength > 4 && xValueLabels.length > 15) {
+                    return
+                }
+                if (labelLength > 12 && xValueLabels.length > 6) {
+                    return
+                } else {
+                    return d;
+                }
+            })
+            .style("font-size", 8);
+
+
+        values_g = categories_g.selectAll(".value" + index)
+            .data(function (d) {
+                return d.values;
+            })
+            .enter().append("g")
+            .attr("class", function (d) {
+                return 'value value-' + d.i;
+            })
+            .attr("transform", function (d) {
+                return "translate(" + lineID(d.key) + ",0)";
+            });
+
+        rects = values_g.selectAll('.rect' + index)
+            .data(function (d) {
+                return [d];
+            })
+            .enter().append("rect")
+            .attr("class", "rect")
+            .attr("width", lineID.rangeBand())
+            .attr("y", function (d) {
+                return y(d.y);
+            })
+            .attr("height", function (d) {
+                return graphSet.height - y(d.y);
+            })
+            .style("fill", function (d) {
+                return graphSet.color(d.key);
+            })
+            .style("opacity", 0.3);
+
+        var hover = categories_g.selectAll('.rect')
+            .data(function (d) {
+                return d.values;
+            })
+            .on("mouseover", function (d) {
+                div.transition()
+                    .style("opacity", 0.9);
+                div.html("<strong>" + d.name + "</strong></br>" + d.y + ": " + d.y_unit + "</br>" + d.x
+                    + ":  " + d.x_unit + "</br>" + "measurement: " + d.measurement)
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function () {
+                div.transition()
+                    .style("opacity", 0);
+            });
+    }
 
 }
 
@@ -222,4 +309,19 @@ function getSum(labels) {
         totalLength += label.length
     });
     return totalLength;
+}
+
+/**
+ *  function takes in nested data by unit type and returns how many units are in data 
+ */
+function howManyUnits(data) {
+    if (data === {}) {
+        return 1
+    }
+     var y_units =  d3.nest()
+        .key(function (d) {
+            return d.y_unit;
+        })
+        .entries(data);
+    return y_units.length;
 }
