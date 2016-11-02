d79d1e66f807e1ba7709096c577cf22835da6543
William Morrell <wcmorrell@lbl.gov>
2016-11-02 12:00:52 -0700

    I think this is a case of Sierra updating sed, where before the flags of -i -e meant 'edit in-place with command', and now they mean 'edit in-place while saving backup with suffix -e'

--- init-config.sh
+++ init-config.sh
@@ -39,18 +39,18 @@ if [ ! -f "$DIR/secrets.env" ]; then
     # replacing the secret{n} values
     while [ $COUNTER -lt 5 ]; do
         EDD_SECRET=`echo "secret${COUNTER} $(date)" | shasum | cut -c 1-32`
-        sed -i -e "s/secret${COUNTER}/${EDD_SECRET}/" "$DIR/secrets.env"
+        sed -i "" -e "s/secret${COUNTER}/${EDD_SECRET}/" "$DIR/secrets.env"
         let COUNTER=COUNTER+1
     done
     # replace Django secret
     EDD_SECRET=`echo "secret${COUNTER} $(date)" | shasum | cut -c 1-32`
-    set -i -e "s/put some random secret text here/${EDD_SECRET}/" "$DIR/secrets.env"
+    sed -i "" -e "s/put some random secret text here/${EDD_SECRET}/" "$DIR/secrets.env"
 fi
 
 if [ ! -f "$DIR/edd/settings/local.py" ]; then
     echo "Copying example local.py settings …"
     cp "$DIR/edd/settings/local.py-example" "$DIR/edd/settings/local.py"
-    sed -i -e "s/'Jay Bay'/'${EDD_USER}'/;s/'admin@example.org'/'${EDD_EMAIL}'/" \
+    sed -i "" -e "s/'Jay Bay'/'${EDD_USER}'/;s/'admin@example.org'/'${EDD_EMAIL}'/" \
         "$DIR/edd/settings/local.py"
 fi
 
