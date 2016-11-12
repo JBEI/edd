009093500871b9dc8fe2cdc50f355214c03b9af0
William Morrell <wcmorrell@lbl.gov>
2016-11-11 17:25:10 -0800

    Moving Dockerfile to docker_services directory; updated requirements.txt with minor versions, and added django-messages-extends

--- edd/urls.py
+++ edd/urls.py
@@ -1,24 +1,30 @@
 from django.conf import settings
 from django.conf.urls import include, url, static
 from django.contrib import admin
-from django.conf import settings
+
 
 admin.autodiscover()
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'edd.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+rest_urlpatterns = [
+    url(r'^rest/', include('edd.rest.urls', namespace='rest')),
+    url(r'^rest/auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
+
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('main.urls', namespace='main')),
-    # url(r'^accounts/', include('django.contrib.auth.urls', namespace='auth')),
     url(r'^accounts/', include('allauth.urls')),  # allauth does not support namespacing
+    url(r'^messages/', include('messages_extends.urls')),
     url(r'^utilities/', include('edd_utils.urls', namespace='edd_utils')),
     url(r'^profile/', include('edd.profile.urls', namespace='profile')),
 ] + static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.PUBLISH_REST_API:
-    urlpatterns.append(url(r'^rest/', include('edd.rest.urls', namespace='rest')))
-    urlpatterns.append(url(r'^rest/auth/', include('rest_framework.urls',
-                               namespace='rest_framework')))
+    urlpatterns += rest_urlpatterns
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
