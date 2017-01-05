6577c8e9faa3717b97959d293b1c9c6ce1b62867
William Morrell <wcmorrell@lbl.gov>
2017-01-05 13:16:18 -0800

    Fixing lint errors in new file

--- jbei/rest/clients/ice/utils.py
+++ jbei/rest/clients/ice/utils.py
@@ -1,12 +1,12 @@
-import collections
-from builtins import enumerate
-from builtins import isinstance
-from builtins import len
+# coding: utf-8
+from __future__ import unicode_literals
 
 
 def make_entry_url(ice_base_url, entry_id):
-    return ('%(base_url)s/entry/%(local_part_id)d' %
+    return (
+        '%(base_url)s/entry/%(local_part_id)d' %
         {
             'base_url': ice_base_url,
             'local_part_id': entry_id,
-        })
+        }
+    )
