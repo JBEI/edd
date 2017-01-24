d19d711b0e9fcd1b1d9510acda1968851988c233
tlopez <tlopez@lbl.gov>
2017-01-24 12:37:13 -0800

    Work in progress for grunt screenshots. Able to successfully take 3/4 of the graph screenshots

--- typescript/src/Study-Overview.ts
+++ typescript/src/Study-Overview.ts
@@ -65,18 +65,16 @@ module StudyOverview {
     // fileRead(), is passed a processed result from the server as a second argument,
     // rather than the raw contents of the file.
     export function fileReturnedFromServer(fileContainer, result): void {
-        //is this needed?
-        $('#fileDropInfoSending').addClass('off');
 
         var currentPath = window.location.pathname;
         var linesPathName = currentPath.slice(0, -8) + 'lines';
         //display success message
         $('#general').append('<div id="successLines" class="success" style="margin-bottom: 17px;">Successfully added ' + result['lines_created'] + ' lines! ' +
-            'Redirecting you to <a style="vertical-align:top" href="/study/{{ study.slug }}/lines">Lines page</a></div>');
+            'Redirecting you to the study details page');
         //redirect to lines page
         setTimeout(function () {
             window.location.pathname = linesPathName;
-        }, 3000);
+        }, 2000);
     }
 
 
