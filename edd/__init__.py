ac75a41465f50af18d4e54d28591d2ffa9eb9019
William Morrell <wcmorrell@lbl.gov>
2017-02-23 17:01:37 -0800

    Monkey-patch the function to cleanse settings for error reporting to catch URLs containing passwords

--- edd/__init__.py
+++ edd/__init__.py
@@ -3,6 +3,10 @@ from __future__ import absolute_import, unicode_literals
 
 import logging
 import os
+import re
+import urlparse
+
+from django.views import debug
 
 # specify a default settings, in case DJANGO_SETTINGS_MODULE env is not set
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edd.settings")
@@ -20,3 +24,22 @@ if hasattr(settings, 'USE_CELERY') and settings.USE_CELERY:
     from .celery import app as celery_app  # noqa
 else:
     logger.info("Celery distributed task queue is not configured")
+
+# monkey-patch django.views.debug.cleanse_setting to check for CELERY_RESULT_BACKEND
+_cleanse_setting = debug.cleanse_setting
+HIDDEN_SETTING = re.compile(r'URL|BACKEND')
+def cleanse_setting(key, value):  # noqa
+    cleansed = _cleanse_setting(key, value)
+    if HIDDEN_SETTING.search(key):
+        try:
+            parsed = urlparse.urlparse(value)
+            if parsed.password:
+                # urlparse returns a read-only tuple, use a list to rewrite parts
+                parsed_list = list(parsed)
+                parsed_list[1] = parsed.netloc.replace(':%s' % parsed.password, ':**********', 1)
+                # put Humpty Dumpty back together again
+                cleansed = urlparse.urlunparse(parsed_list)
+        except:
+            logger.exception('Exception cleansing URLs for error reporting')
+    return cleansed
+debug.cleanse_setting = cleanse_setting
