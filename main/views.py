18c6e4cbd15b515b7064e60c5bc4796a1a97a1fe
Mark Forrer <mark.forrer@lbl.gov>
2017-03-17 16:40:21 -0700

    Use POST parameters instead of placeholder request headers to take input from the UI

--- main/views.py
+++ main/views.py
@@ -1265,9 +1265,9 @@ def study_describe_experiment(request, pk=None, slug=None):
 
     # parse request parameter input to keep subsequent code relatively format-agnostic
     user = request.user
-    dry_run = 'dryRun' in request.META
-    allow_duplicate_names = ALLOW_DUPLICATE_NAMES_PARAM in request.META
-    ignore_ice_related_errors = IGNORE_ICE_RELATED_ERRORS_PARAM in request.META
+    dry_run = 'dryRun' in request.POST
+    allow_duplicate_names = ALLOW_DUPLICATE_NAMES_PARAM in request.POST
+    ignore_ice_related_errors = IGNORE_ICE_RELATED_ERRORS_PARAM in request.POST
 
     # detect the input format
     has_file_type = FILE_TYPE_HEADER in request.META
