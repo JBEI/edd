22d6585a0c24ec808443a3fff3725c5aed1d7fd4
Mark Forrer <mark.forrer@lbl.gov>
2017-04-19 11:49:08 -0700

    Improved test_ice_communication to optionally accept a part ID to help with targeted ICE integration testing. Used successfully during today’s testing related to Jesus’ ED file.

--- main/management/commands/test_ice_communication.py
+++ main/management/commands/test_ice_communication.py
@@ -14,7 +14,12 @@ class Command(BaseCommand):
     _USERNAME_ARG = 'username'
 
     def add_arguments(self, parser):
-        parser.add_argument('username')
+        parser.add_argument('username', help='The username of the ICE user whose credentials '
+                                             'will be used to access the system')
+        parser.add_argument('--part_id', help='The optional part id, local primary key, '
+                                              'or UUID for a single ICE part to be accessed. If '
+                                              'not present, a generic search for parts will be '
+                                              'performed instead.')
 
     def handle(self, *args, **options):
         # get the username argument
@@ -28,18 +33,30 @@ class Command(BaseCommand):
 
         try:
             print('Contacting ICE at %s' % ice.base_url)
-            entries_results_page = ice.search_entries()
-            if entries_results_page:
-                self.stdout.write(
-                    'Successfully searched ICE for entries.\nICE reports %(total_entries)d total '
-                    'entries and returned the first page of  %(returned_entries)d' % {
-                        'total_entries': entries_results_page.total_result_count,
-                        'returned_entries': entries_results_page.current_result_count,
-                    }
-                )
+
+            part_id = options.get('part_id')
+
+            if part_id:
+                print('Requesting part "%s"' % part_id)
+                entry = ice.get_entry(part_id)
+                if entry:
+                   print('Found the part!')
+                else:
+                    print('Part "%s" was not found in ICE' % part_id)
             else:
-                print("No known error occurred, but also didn't find any entries in ICE. If user "
-                      "%s has any ICE entries visible to him/her, you might want to look into "
-                      "this." % username)
+
+                entries_results_page = ice.search_entries()
+                if entries_results_page:
+                    self.stdout.write(
+                        'Successfully searched ICE for entries.\nICE reports %(total_entries)d total '
+                        'entries and returned the first page of  %(returned_entries)d' % {
+                            'total_entries': entries_results_page.total_result_count,
+                            'returned_entries': entries_results_page.current_result_count,
+                        }
+                    )
+                else:
+                    print("No known error occurred, but also didn't find any entries in ICE. If user "
+                          "%s has any ICE entries visible to him/her, you might want to look into "
+                          "this." % username)
         except Exception as e:
             raise CommandError(e)
