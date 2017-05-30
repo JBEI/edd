dfe3775f9df573a02d6c0de3c41b5cfe06573f33
tlopez <tlopez@lbl.gov>
2017-05-30 09:42:58 -0700

    import.js tests pass.

--- main/static/main/js/test/assay_table_data_spec.js
+++ main/static/main/js/test/assay_table_data_spec.js
@@ -1,26 +1,28 @@
-/**
- * unit tests for AssayTableData.js
+ /* unit tests for Import.js
  */
 
 describe("Import.js", function () {
-    var selectMajorKindStep;
+     var selectMajorKindStep, eddTableImport, rawInput, standardProcessor, importMessage;
     beforeEach(function() {
-       jasmine.getJSONFixtures().fixturesPath='base/main/static/main/js/test/';
-       EDDData = getJSONFixture('EDDData.json');
-       selectMajorKindStep = new EDDTableImport.SelectMajorKindStep();
-    });
-    describe('method: SelectMajorKindStep.checkInterpretationMode', function() {
-        it ('should initially return false', function() {
-             expect(selectMajorKindStep.checkInterpretationMode()).toBeFalsy()
-        })
+        jasmine.getFixtures().fixturesPath='base/main/static/main/js/test/';
+        jasmine.getJSONFixtures().fixturesPath='base/main/static/main/js/test/';
+        loadFixtures('SpecRunner.html');
+        EDDData = getJSONFixture('EDDData.json');
+        importMessage = new EDDTableImport.ImportMessage();
+        selectMajorKindStep = new EDDTableImport.SelectMajorKindStep();
     });
     describe('method: SelectMajorKindStep.checkMasterProtocol', function() {
-        it ('should initially return false', function() {
+         it ('should initially return false', function() {
             //this should be returning false..
             expect(selectMajorKindStep.checkMasterProtocol()).toBeTruthy()
         })
     });
-     describe('method: SelectMajorKindStep.getUserWarnings', function() {
+    describe('method: SelectMajorKindStep.checkMasterProtocol', function() {
+        it ('should return an empty array', function() {
+            expect(selectMajorKindStep.checkMasterProtocol()).toBeTruthy()
+        })
+    });
+    describe('method: SelectMajorKindStep.getUserWarnings', function() {
         it ('should return an empty array', function() {
             expect(selectMajorKindStep.getUserWarnings()).toEqual([])
         })
