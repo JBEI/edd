bf5c94c9be73c6dc4ebf3fd74679fb7ce9171560
William Morrell <wcmorrell@lbl.gov>
2017-07-12 16:10:33 -0700

    Converting code parsing SBML templates to a celery task; should fix EDD-884

--- main/tasks.py
+++ main/tasks.py
@@ -152,3 +152,41 @@ def unlink_ice_entry_from_study(self, user_token, strain, study):
             raise self.retry(exc=e, countdown=delay_calculation(self), max_retries=10)
         except Exception as e:
             raise e
+
+
+@shared_task
+def template_sync_species(template_id):
+    """
+    Task parses an SBML document, then creates MetaboliteSpecies and MetaboliteExchange records
+    for every species and single-reactant reaction in the model.
+    """
+    instance = models.SBMLTemplate.objects.get(pk=template_id)
+    doc = instance.parseSBML()
+    model = doc.getModel()
+    # filter to only those for the updated template
+    species_qs = models.MetaboliteSpecies.objects.filter(sbml_template=instance)
+    exchange_qs = models.MetaboliteExchange.objects.filter(sbml_template=instance)
+    exist_species = set(species_qs.values_list('species', flat=True))
+    exist_exchange = set(exchange_qs.values_list('exchange_name', flat=True))
+    # creating any records not in the database
+    for species in map(lambda s: s.getId(), model.getListOfSpecies()):
+        if species not in exist_species:
+            models.MetaboliteSpecies.objects.get_or_create(
+                sbml_template=instance,
+                species=species
+            )
+        else:
+            exist_species.discard(species)
+    reactions = map(lambda r: (r.getId(), r.getListOfReactants()), model.getListOfReactions())
+    for reaction, reactants in reactions:
+        if len(reactants) == 1 and reaction not in exist_exchange:
+            models.MetaboliteExchange.objects.get_or_create(
+                sbml_template=instance,
+                exchange_name=reaction,
+                reactant_name=reactants[0].getSpecies()
+            )
+        else:
+            exist_exchange.discard(reaction)
+    # removing any records in the database not in the template document
+    species_qs.filter(species__in=exist_species).delete()
+    exchange_qs.filter(exchange_name__in=exist_exchange).delete()
