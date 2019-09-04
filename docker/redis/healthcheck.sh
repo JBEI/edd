479c1fe75c7bdba0250481385c419c5de0c8ae8f
William Morrell <WCMorrell@lbl.gov>
2019-09-04 16:19:36 -0700

    Merge pull request #555 in EDD/edd-django from ~WCMORRELL/edd:feature/EDD-1171-Redis-upgrade to master
    Squashed commit of the following:
    
    commit 341eab6a38dcdafd7476c1d7eb34594b0554030a
    Author: William Morrell <WCMorrell@lbl.gov>
    Date:   Wed Sep 4 16:00:38 2019 -0700
    
        Upgrade Redis image to 5-alpine tag from 3.2

--- docker/redis/healthcheck.sh
+++ docker/redis/healthcheck.sh
@@ -1,13 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 # Based on healthcheck script found at:
 # https://github.com/docker-library/healthcheck/blob/master/redis/docker-healthcheck
-
-set -eo pipefail
-
-host="$(hostname --ip-address || echo '127.0.0.1')"
-
-if ping="$(redis-cli -h "$host" ping)" && [ "$ping" = 'PONG' ]; then
+host="$(hostname -i || echo '127.0.0.1')"
+if [ "$(redis-cli -h "$host" ping)" = 'PONG' ]; then
     exit 0
 fi
 
