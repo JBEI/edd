c98695653cd29c58bbef5247741bbb615c73040f
William Morrell <WCMorrell@lbl.gov>
2020-04-27 21:01:09 -0700

    Make linter happy with webpack.config.js
    My editor was being very angry about the lack of quotes on object keys, the use
    of var, and spacing that passes prettier muster.
    
    No functional changes, this is purely a formatting commit.

--- typescript/webpack.config.js
+++ typescript/webpack.config.js
@@ -1,35 +1,35 @@
 /**
  * Created by tlopez on 6/13/17.
  */
-
+/* eslint @typescript-eslint/no-var-requires: off */
 // NOTE: to get a path relative to package.json, use path.resolve("node_modules")
 //  to get a path relative to webpack.config.js, use path.resolve(__dirname, "rel/path")
-var path = require("path");
-var webpack = require("webpack");
+const path = require("path");
+const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-var css_extract = new MiniCssExtractPlugin({
+const css_extract = new MiniCssExtractPlugin({
     "allChunks": true,
     "chunkFilename": "[name].css",
     "filename": "styles.css",
 });
 
 module.exports = {
-    entry: {
-        Admin: path.resolve(__dirname, "./src/Admin.ts"),
-        Campaign: path.resolve(__dirname, "./src/Campaign.ts"),
-        CampaignIndex: path.resolve(__dirname, "./src/Campaign-Index.ts"),
-        Common: [
+    "entry": {
+        "Admin": path.resolve(__dirname, "./src/Admin.ts"),
+        "Campaign": path.resolve(__dirname, "./src/Campaign.ts"),
+        "CampaignIndex": path.resolve(__dirname, "./src/Campaign-Index.ts"),
+        "Common": [
             path.resolve("node_modules/bootstrap/dist/js/bootstrap"),
             path.resolve("node_modules/bootstrap/dist/css/bootstrap.min.css"),
             path.resolve(__dirname, "./src/Common.ts"),
         ],
-        Cytometry: path.resolve(__dirname, "./src/Cytometry.ts"),
-        ExperimentDescHelp: path.resolve(__dirname, "./src/Experiment-Desc-Help.ts"),
-        Export: path.resolve(__dirname, "./src/Export.ts"),
-        GCMS_Workbench: path.resolve(__dirname, "./src/GCMS_Workbench.ts"),
-        Import: path.resolve(__dirname, "./src/Import.ts"),
-        Import2: [
+        "Cytometry": path.resolve(__dirname, "./src/Cytometry.ts"),
+        "ExperimentDescHelp": path.resolve(__dirname, "./src/Experiment-Desc-Help.ts"),
+        "Export": path.resolve(__dirname, "./src/Export.ts"),
+        "GCMS_Workbench": path.resolve(__dirname, "./src/GCMS_Workbench.ts"),
+        "Import": path.resolve(__dirname, "./src/Import.ts"),
+        "Import2": [
             "react",
             "react-dom",
             "react-dropzone",
@@ -37,35 +37,38 @@ module.exports = {
             "react-stepzilla.css",
             path.resolve(__dirname, "./src/Import2.tsx"),
         ],
-        Import2Help: path.resolve(__dirname, "./src/Import2-Help.tsx"),
-        index: path.resolve(__dirname, "./src/index.ts"),
-        RNASeq: path.resolve(__dirname, "./src/RNASeq.ts"),
-        Skyline_Convert: path.resolve(__dirname, "./src/Skyline_Convert.ts"),
-        StudyData: path.resolve(__dirname, "./src/Study-Data.ts"),
-        StudyLines: path.resolve(__dirname, "./src/Study-Lines.ts"),
-        StudyLinesAddCombos: path.resolve(__dirname, "./src/Study-Lines-Add-Combos.ts"),
-        StudyOverview: path.resolve(__dirname, "./src/Study-Overview.ts"),
+        "Import2Help": path.resolve(__dirname, "./src/Import2-Help.tsx"),
+        "index": path.resolve(__dirname, "./src/index.ts"),
+        "RNASeq": path.resolve(__dirname, "./src/RNASeq.ts"),
+        "Skyline_Convert": path.resolve(__dirname, "./src/Skyline_Convert.ts"),
+        "StudyData": path.resolve(__dirname, "./src/Study-Data.ts"),
+        "StudyLines": path.resolve(__dirname, "./src/Study-Lines.ts"),
+        "StudyLinesAddCombos": path.resolve(
+            __dirname,
+            "./src/Study-Lines-Add-Combos.ts",
+        ),
+        "StudyOverview": path.resolve(__dirname, "./src/Study-Overview.ts"),
     },
-    output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname, "../server/main/static/dist"),
-        publicPath: "/static/dist/",
+    "output": {
+        "filename": "[name].js",
+        "path": path.resolve(__dirname, "../server/main/static/dist"),
+        "publicPath": "/static/dist/",
     },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    chunks: "all",
-                    name: "vendor",
-                    test: /[\\/]node_modules[\\/](?!react*).*/,
+    "optimization": {
+        "splitChunks": {
+            "cacheGroups": {
+                "vendor": {
+                    "chunks": "all",
+                    "name": "vendor",
+                    "test": /[\\/]node_modules[\\/](?!react*).*/,
                 },
             },
         },
     },
-    resolve: {
-        modules: [path.resolve("node_modules"), path.resolve(__dirname, "modules")],
-        extensions: [".js", ".json", ".jsx", ".css", ".ts", ".vue"],
-        alias: {
+    "resolve": {
+        "modules": [path.resolve("node_modules"), path.resolve(__dirname, "modules")],
+        "extensions": [".js", ".json", ".jsx", ".css", ".ts", ".vue"],
+        "alias": {
             "handsontable": path.resolve(
                 "node_modules/handsontable/dist/handsontable.full.js",
             ),
@@ -77,19 +80,19 @@ module.exports = {
             ),
         },
     },
-    module: {
-        rules: [
+    "module": {
+        "rules": [
             // define loader for Typescript files
             {
-                test: /\.tsx?$/,
-                loader: "ts-loader",
+                "test": /\.tsx?$/,
+                "loader": "ts-loader",
             },
             // define loader that also runs eslint for our files
             {
-                enforce: "pre",
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                loader: "eslint-loader",
+                "enforce": "pre",
+                "test": /\.tsx?$/,
+                "exclude": /node_modules/,
+                "loader": "eslint-loader",
                 // uncomment below to do auto-fix in local builds
                 // options: {
                 //   fix: true,
@@ -97,27 +100,27 @@ module.exports = {
             },
             // define loader for stylesheets
             {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
+                "test": /\.css$/,
+                "use": [MiniCssExtractPlugin.loader, "css-loader"],
             },
             // define loader for images
             {
-                test: /\.(jpe?g|png|gif|ico)$/,
-                loader: "file-loader",
+                "test": /\.(jpe?g|png|gif|ico)$/,
+                "loader": "file-loader",
             },
             // define loader for fonts, etc
             {
-                test: /\.(woff|woff2|eot|ttf|svg)$/,
-                loader: "url-loader",
-                options: {
-                    limit: 8192,
+                "test": /\.(woff|woff2|eot|ttf|svg)$/,
+                "loader": "url-loader",
+                "options": {
+                    "limit": 8192,
                 },
             },
         ],
-        noParse: [/handsontable\.full(\.min)?\.js/],
+        "noParse": [/handsontable\.full(\.min)?\.js/],
     },
-    devtool: "source-map",
-    plugins: [
+    "devtool": "source-map",
+    "plugins": [
         new webpack.DefinePlugin({
             "process.env.NODE_ENV": '"production"',
         }),
