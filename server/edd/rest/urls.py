6a217c85b88cd22dc0adfb45e5fdf831742c734a
William Morrell <WCMorrell@lbl.gov>
2020-10-29 16:17:04 -0700

    Add JWT login option to REST API
    The REST API previously only worked with Session logins, requiring the HTTP
    client to handle cookies to function, and logging in via the same URLs used for
    normal web logins. Adding the SimpleJWT package and a few extra URL routes adds
    the ability to login at /rest/token/ and use the access token returned in an
    Authorization header. Should add some text to the template rendering REST
    documentation, calling out the /rest/token/ APIs, and how to convert the result
    into a header value with e.g. curl.

--- server/edd/rest/urls.py
+++ server/edd/rest/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from rest_framework_simplejwt import views as jwt
 
 from . import routers, views
 
@@ -12,4 +13,7 @@ urlpatterns = [
     path(r"", include(routers.base_rest_api_router.urls)),
     path(r"", include(routers.study_router.urls)),
     path(r"docs/", views.schema_view, name="docs"),
+    path(r"token/", jwt.TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path(r"token/refresh/", jwt.TokenRefreshView.as_view(), name="token_refresh"),
+    path(r"token/verify/", jwt.TokenVerifyView.as_view(), name="token_verify"),
 ]
