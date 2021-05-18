bc474d9c5d8e1d7e5881200344e4b356367fbdc1
William Morrell <WCMorrell@lbl.gov>
2021-05-18 11:56:11 -0700

    Fix loading of ASGI application
    The upgraded Channels library makes some changes to how ASGI applications are
    loaded. Updating our code to more closely match what the 3.x documentation
    shows. Wrapping the imports of EDD custom code and AuthMiddlewareStack in a
    function, as the default example shown in the documentation will have imports
    re-organized in a way that will break the application.

--- server/edd/notify/routing.py
+++ server/edd/notify/routing.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import consumers
 
 url_patterns = [
-    path("ws/notify/", consumers.NotifySubscribeConsumer),
+    path("ws/notify/", consumers.NotifySubscribeConsumer.as_asgi()),
 ]
 
 __all__ = [url_patterns]
