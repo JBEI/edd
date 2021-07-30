2c23bebfa6850c2d6344f70704c17906dedce948
William Morrell <WCMorrell@lbl.gov>
2021-07-29 22:52:26 -0700

    Squash profile app migrations
    Doing this will eliminate use of the old HSTORE method of storing a dictionary
    type in a column. Use of HSTORE was replaced with a JSONB column, but the
    migrations still reference HSTORE, and require any new database deployments to
    have the HSTORE module enabled.

--- docker/postgres/initdb/100_init.sql
+++ docker/postgres/initdb/100_init.sql
@@ -3,11 +3,6 @@
 -- Create edduser with environment password
 CREATE USER edduser WITH PASSWORD :'edd_pgpass'
     NOSUPERUSER INHERIT CREATEDB NOCREATEROLE NOREPLICATION;
-\c template1
--- Enable the hstore extension in the template database
-CREATE EXTENSION IF NOT EXISTS "hstore";
--- Enable the uuid extension in the template database
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 -- Create the database for EDD; will have hstore/uuid since template already created it
 CREATE DATABASE edd;
 -- Ensure edduser role can access edd database
