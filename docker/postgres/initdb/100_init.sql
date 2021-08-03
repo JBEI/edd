09b6e02eaed3c864d28ca5f2b1f1f419094ac6ea
William Morrell <WCMorrell@lbl.gov>
2021-08-02 17:25:17 -0700

    Update setup to use Postgres 11
    We're using Postgres 11 now in production, so it only makes sense to update
    development environments to use it as well. Eliminating the custom container,
    and instead using the official Postgres container. Any existing development
    deployments can continue using a compose file with Postgres 9.6; or, update to
    use Postgres 11 on a new volume.
    
    Changes to make in compose override file:
    -   Switch `postgres` and `ice_db` keys for `image` to `library/postgres:11`
    -   Add environment for `POSTGRES_DB=edd` and `POSTGRES_USER=edduser`
    -   Change `postgres.env` to use value of `EDD_PGPASS` for `POSTGRES_PASSWORD`

--- docker/postgres/initdb/100_init.sql
+++
@@ -1,9 +0,0 @@
--- Load a password from the environment, or use default value 'jbei'
-\set edd_pgpass `echo "${EDD_PGPASS:-jbei}"`
--- Create edduser with environment password
-CREATE USER edduser WITH PASSWORD :'edd_pgpass'
-    NOSUPERUSER INHERIT CREATEDB NOCREATEROLE NOREPLICATION;
--- Create the database for EDD; will have hstore/uuid since template already created it
-CREATE DATABASE edd;
--- Ensure edduser role can access edd database
-GRANT ALL PRIVILEGES ON DATABASE edd TO edduser;
