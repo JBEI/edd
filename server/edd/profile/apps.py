7b822462071bc4a7db178bbe253a3afe1acf0c72
William Morrell <WCMorrell@lbl.gov>
2021-08-31 22:47:54 -0700

    Change to custom user model
    Replacing the monkey-patched functions on the User object with a real custom
    User class that extends the default Django AbstractUser class. This makes the
    application setup slightly simpler, because we don't need to hook in to add the
    monkey-patched functions at the right time anymore; and Django will no longer
    detect pending migrations for the django.contrib.auth app anymore because of
    our custom Manager object that fetches the Profile associated with a user.
    
    Changes here are roughly following the outline given in:
    https://www.caktusgroup.com/blog/2019/04/26/how-switch-custom-django-user-model-mid-project/
    and
    https://code.djangoproject.com/ticket/25313
    
    Since we don't care much about the table name in the database, using the same
    auto-generated name in the standard Django model is fine. Since we also have a
    freshly-squashed migration in the edd.profile app, and that app also deals with
    User-related things, adding in the definition for the new User model there
    allows for everything to continue functioning exactly as before for existing
    installations, and still work for brand-new installations.

--- server/edd/profile/apps.py
+++ server/edd/profile/apps.py
@@ -5,11 +5,3 @@ class ProfileConfig(AppConfig):
     label = "profile"
     name = "edd.profile"
     verbose_name = "User Profiles"
-
-    def ready(self):
-        from django.contrib.auth import get_user_model
-
-        from .models import patch_user_model
-
-        # before loading anything else, add our patches to the User object
-        patch_user_model(get_user_model())
--- server/main/admin.py
+++ server/main/admin.py
@@ -4,8 +4,6 @@ from celery import shared_task
 from django import forms
 from django.conf import settings
 from django.contrib import admin, messages
-from django.contrib.auth import get_user_model, hashers
-from django.contrib.auth.admin import UserAdmin
 from django.core.validators import RegexValidator
 from django.db import connection
 from django.db.models import Count, Q
@@ -15,13 +13,10 @@ from django.urls import reverse
 from django.utils.html import escape, format_html
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
-from django_auth_ldap.backend import LDAPBackend
 
 from edd.export.sbml import validate_sbml_attachment
-from edd.search.solr import StudySearch, UserSearch
+from edd.search.solr import StudySearch
 from edd.utilities import S3MediaStorage
-from jbei.rest.auth import HmacAuth
-from jbei.rest.clients.ice import IceApi, IceApiException
 
 from . import models
 from .forms import (
@@ -821,131 +816,6 @@ class SBMLTemplateAdmin(EDDObjectAdmin):
         return exchange_name
 
 
-class UserHasLocalLoginFilter(admin.SimpleListFilter):
-
-    title = _("Has Local Login")
-    parameter_name = "localauth"
-
-    def lookups(self, request, model_admin):
-        return (("T", _("Has Local")),)
-
-    def queryset(self, request, queryset):
-        # ignoring the system user and inactive accounts
-        active = queryset.exclude(username="system").exclude(is_active=False)
-        if self.value() == "T":
-            return active.exclude(password__startswith="!")
-        return queryset
-
-
-class EDDUserAdmin(UserAdmin):
-    """ Definition for admin-edit of user accounts """
-
-    # actions is a list
-    actions = UserAdmin.actions + [
-        "solr_index",
-        "update_groups_from_ldap",
-        "search_ice_as_action",
-        "deactivate_user_action",
-        "migrate_local_to_ldap",
-    ]
-    # list_display is a tuple
-    list_display = UserAdmin.list_display + ("date_joined", "last_login")
-    list_filter = UserAdmin.list_filter + (UserHasLocalLoginFilter,)
-
-    def solr_index(self, request, queryset):
-        solr = UserSearch()
-        # optimize queryset to fetch profile with JOIN, and single
-        # queries for group/institutions instead of one per record
-        q = queryset.select_related("userprofile")
-        q = q.prefetch_related("groups", "userprofile__institutions")
-        solr.update(q)
-
-    solr_index.short_description = _("Index in Solr")
-
-    def update_groups_from_ldap(self, request, queryset):
-        backend = LDAPBackend()
-        for user in queryset:
-            ldap_user = backend.get_user(user.pk)
-            try:
-                ldap_user.ldap_user._mirror_groups()
-            except Exception:
-                # _mirror_groups fails when ldap_user is not Active, so delete all groups
-                user.groups.clear()
-
-    update_groups_from_ldap.short_description = _("Update Groups from LDAP")
-
-    def search_ice_as_action(self, request, queryset):
-        # intentionally throw error when multiple users selected
-        user = queryset.get()
-        context = self.admin_site.each_context(request)
-        auth = HmacAuth(key_id=settings.ICE_KEY_ID, username=user.email)
-        ice = IceApi(auth=auth, verify_ssl_cert=settings.ICE_VERIFY_CERT)
-        ice.timeout = settings.ICE_REQUEST_TIMEOUT
-        results = []
-        term = request.POST.get("term", None)
-        if term is not None:
-            try:
-                results = ice.search(term)
-            except IceApiException:
-                self.message_user(
-                    request,
-                    _("Failed to execute search in ICE, check the ICE logs."),
-                    messages.ERROR,
-                )
-        context.update(ice=ice.base_url, results=results, impersonate=user)
-        return render(request, "admin/strain_impersonate_search.html", context=context)
-
-    search_ice_as_action.short_description = _("Search ICE as User")
-
-    def deactivate_user_action(self, request, queryset):
-        try:
-            count = queryset.update(is_active=False)
-            self.message_user(
-                request,
-                _("Deactivated {count} users").format(count=count),
-                messages.SUCCESS,
-            )
-        except Exception as e:
-            logger.exception(f"User deactivation failed {e}")
-            self.message_user(
-                request,
-                _("Failed to deactivate users, check the EDD logs"),
-                messages.ERROR,
-            )
-
-    deactivate_user_action.short_description = _("Deactivate Users")
-
-    def migrate_local_to_ldap(self, request, queryset):
-        backend = LDAPBackend()
-        for user in queryset:
-            # annotate with ldap_user
-            user = backend.get_user(user.pk)
-            try:
-                if user.ldap_user.dn is not None:
-                    # replace local password with an invalid one
-                    user.password = hashers.make_password(None)
-                    user.save(update_fields=["password"])
-                    # populate local record with LDAP values
-                    user.ldap_user.populate_user()
-                else:
-                    self.message_user(
-                        request,
-                        _("Did not find matching LDAP record for {user}").format(
-                            user=user.username
-                        ),
-                        messages.WARNING,
-                    )
-            except Exception as e:
-                logger.exception(f"User migration to LDAP account failed {e}")
-                self.message_user(
-                    request,
-                    _("Failed to migrate {user}").format(user=user.username),
-                    messages.ERROR,
-                )
-
-    migrate_local_to_ldap.short_description = _("Migrate account to LDAP")
-
-
 class WorklistColumnInline(admin.TabularInline):
     """ Inline submodel for editing worklist columns. """
 
@@ -986,6 +856,3 @@ admin.site.register(models.SBMLTemplate, SBMLTemplateAdmin)
 admin.site.register(models.Strain, StrainAdmin)
 admin.site.register(models.Study, StudyAdmin)
 admin.site.register(models.WorklistTemplate, WorklistTemplateAdmin)
-
-admin.site.unregister(get_user_model())
-admin.site.register(get_user_model(), EDDUserAdmin)
