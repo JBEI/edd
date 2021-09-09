e925b08c029e218accc2369abbcf819cb4f2e0e1
William Morrell <WCMorrell@lbl.gov>
2021-09-09 16:45:40 -0700

    Refactor registration of Celery task serializer
    Moving the registration of our custom JSON serialization classes to just prior
    to the initialization of the Celery app. Delaying the registration until the
    `edd.utilities` module is imported would sometimes result in the serializers
    not getting registered in time for use by Celery. The problem goes away if it
    becomes impossible to initialize Celery before the registration.

--- server/edd/celery.py
+++ server/edd/celery.py
@@ -6,9 +6,26 @@ the Celery cluster.
 import os
 
 from celery import Celery
+from kombu.serialization import register
 
 # Ensure that there is at least a default settings module configured, using EDD settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edd.settings")
+
+
+def register_json_serialization():
+    from .utilities import JSONDecoder, JSONEncoder
+
+    # register our custom JSON serialization code
+    register(
+        name="edd-json",
+        encoder=JSONEncoder.dumps,
+        decoder=JSONDecoder.loads,
+        content_type="application/x-edd-json",
+        content_encoding="UTF-8",
+    )
+
+
+register_json_serialization()
 
 # set up a Celery "app" for use by EDD. A Celery "app" is an instance of
 # the Celery API, this instance defines EDD's interface with Celery.
