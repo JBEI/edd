cf74a31c956ba85a051f0410306506db547eee55
William Morrell <WCMorrell@lbl.gov>
2021-09-28 12:16:52 -0700

    Remove providing_args argument to Signal instances
    The argument is removed and deprecated from Django. Eliminating it silences the
    warnings about the deprecation.

--- server/edd/load/signals.py
+++ server/edd/load/signals.py
@@ -1,11 +1,7 @@
 """Signals and handlers for loading data into EDD."""
 
-import logging
-
 import django
 
-logger = logging.getLogger(__name__)
-
 # create signals for errors and warnings
-errors_reported = django.dispatch.Signal(providing_args=["key", "errors"])
-warnings_reported = django.dispatch.Signal(providing_args=["key", "warnings"])
+errors_reported = django.dispatch.Signal()
+warnings_reported = django.dispatch.Signal()
--- server/main/signals/signals.py
+++ server/main/signals/signals.py
@@ -1,18 +1,18 @@
 from django.dispatch import Signal
 
 # Search index management signals
-study_modified = Signal(providing_args=["study", "using"])
-study_removed = Signal(providing_args=["doc", "using"])
-type_modified = Signal(providing_args=["measurement_type", "using"])
-type_removed = Signal(providing_args=["doc", "using"])
-user_modified = Signal(providing_args=["user", "using"])
-user_removed = Signal(providing_args=["doc", "using"])
+study_modified = Signal()
+study_removed = Signal()
+type_modified = Signal()
+type_removed = Signal()
+user_modified = Signal()
+user_removed = Signal()
 
 # EDD efficiency metric signals
-study_created = Signal(providing_args=("study", "user"))
-study_described = Signal(providing_args=("study", "user", "count"))
-study_exported = Signal(providing_args=("study", "user", "count"))
-study_imported = Signal(providing_args=("study", "user", "protocol", "count"))
-study_permission_change = Signal(providing_args=("study", "user", "permission"))
-study_viewed = Signal(providing_args=("study", "user"))
-study_worklist = Signal(providing_args=("study", "user", "count"))
+study_created = Signal()
+study_described = Signal()
+study_exported = Signal()
+study_imported = Signal()
+study_permission_change = Signal()
+study_viewed = Signal()
+study_worklist = Signal()
