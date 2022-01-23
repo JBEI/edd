d97fcd02d5d90fd5a98100e40b709b2e3466fa0b
Somtirtha Roy <somtirtharoy@lbl.gov>
2022-01-22 16:02:36 -0800

    Fix issues with importing large files
    - Changed all the parsed data from ading to a worksheet to being in a list
    - changed the way the fiels are read and parsd to improve performance
    - changed from using iter_cols to using only rows method in the spread sheet
    - changed to read_only=True to allow importing large files

--- server/edd/load/parsers/ambr.py
+++ server/edd/load/parsers/ambr.py
@@ -2,7 +2,6 @@ import logging
 from uuid import UUID
 
 import numpy as np
-from openpyxl import Workbook
 
 from edd.load.models import DefaultUnit, MeasurementNameTransform
 from main.models import MeasurementType
@@ -16,19 +15,19 @@ logger = logging.getLogger(__name__)
 class AmbrExcelParser(MultiSheetExcelParserMixin, GenericImportParser):
     def __init__(self, import_uuid: UUID):
         super().__init__(import_uuid=import_uuid,)
-        # self.parsed_result = pd.DataFrame()
-        self.workbook = Workbook()
-        self.worksheet = self.workbook.active
+        self.parsed_sheet_rows = []
 
     def _parse_sheet_rows(self, name, sheet):
 
         # for every two columns in the worksheet
         # corresponding to each measurement type in the sheet
-        for index in range(1, sheet.max_column + 1, 2):
-            for col in sheet.iter_cols(min_row=1, min_col=index, max_col=index):
-                time_data = [el.value for el in col]
-            for col in sheet.iter_cols(min_row=1, min_col=index + 1, max_col=index + 1):
-                mes_data = [el.value for el in col]
+        for col_index in range(1, sheet.max_column + 1, 2):
+            time_data = []
+            mes_data = []
+            for row in sheet.rows:
+                row_list = [cell.value for cell in row][col_index - 1 : col_index + 1]
+                time_data.append(row_list[0])
+                mes_data.append(row_list[1])
 
             # decimate the data here
             # check if data has more than 200 points then decimate else do not
@@ -52,7 +51,8 @@ class AmbrExcelParser(MultiSheetExcelParserMixin, GenericImportParser):
             mes_transform_qs = MeasurementNameTransform.objects.all().filter(
                 input_type_name=mtype_name, parser="ambr"
             )
-            mtype_name = mes_transform_qs[0].edd_type_name.type_name
+            if mes_transform_qs:
+                mtype_name = mes_transform_qs[0].edd_type_name.type_name
         except Exception as ex:
             logger.debug(
                 f"Error trying to retrieve measurement type mapping \
@@ -85,14 +85,18 @@ class AmbrExcelParser(MultiSheetExcelParserMixin, GenericImportParser):
         # appending mapped measurements to parsed worksheet
         for i in range(1, len(mes_data)):
             # dropping records with NaN values
-            if mes_data[i] is not None:
-                if not np.isnan(float(mes_data[i])):
-                    self.worksheet.append(
-                        [
-                            name,
-                            mtype_name,
-                            float(mes_data[i]),
-                            float(time_data[i]),
-                            du_obj.unit.unit_name,
-                        ]
+            if self.is_valid(mes_data[i]):
+                self.parsed_sheet_rows.append(
+                    (
+                        name,
+                        mtype_name,
+                        float(mes_data[i]),
+                        float(time_data[i]),
+                        du_obj.unit.unit_name,
                     )
+                )
+
+    def is_valid(self, value):
+        if value is not None and not np.isnan(float(value)):
+            return True
+        return False
