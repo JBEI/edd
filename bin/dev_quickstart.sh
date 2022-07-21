8d48e51bc4f1f74bd8dfa5927b8df353a4491d8c
William Morrell <WCMorrell@lbl.gov>
2022-07-21 13:19:56 -0700

    Add a step to copy default settings in setup
    Using a non-localhost address will not work with the baseline setting, but the
    example uses a wildcard to allow any host header.

--- bin/dev_quickstart.sh
+++ bin/dev_quickstart.sh
@@ -10,3 +10,5 @@ DOCKER_BUILDKIT=1 docker build -t jbei/edd-config .
 cd "${EDD_ROOT}"
 echo 'Creating initial configration scripts'
 bin/init-config offline --deploy=dev
+echo 'Creating default (dev-only!) settings'
+cp settings/example settings/__init__.py
