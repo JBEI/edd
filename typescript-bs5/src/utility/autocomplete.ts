c10240faa8c206726cbbbeb71f4460f698613bcf
William Morrell <WCMorrell@lbl.gov>
2023-01-18 22:25:14 -0800

    Fix autofocus on search field with autocompletes
    There is an unintended interaction between JQuery's handling of focus events
    and the way Select2 works, where with specific versions of each library, the
    search field part of an autocomplete will fail to focus. To work around the
    problem, we can hook into the event where an autocomplete is opened, and use
    that to trigger a native browser API focus event, skipping over JQuery's
    handling of events.
    
    Presumably, the libraries will update to fix this problem eventually. The
    additional event handler and the forceFocus function should be removed once the
    fix is merged upstream.
    
    See: https://github.com/select2/select2/issues/5993

--- typescript-bs5/src/utility/autocomplete.ts
+++ typescript-bs5/src/utility/autocomplete.ts
@@ -3,6 +3,11 @@
 import "jquery";
 import "select2";
 
+/**
+ * Prepare any autocomplete inputs to use Select2 for querying the EDD backend
+ * APIs for matching results. If no items to initialize are provided as an
+ * argument, the function will setup any elements with the "autocomp2" class.
+ */
 export function initSelect2(elements?: JQuery): void {
     // when nothing passed, initialize all elements with autocomp2 class
     if (elements === undefined) {
@@ -44,3 +49,18 @@ function entryTemplate(payload: any): string | JQuery {
     }
     return payload?.text || "";
 }
+
+/**
+ * Hacky fix for a bug in select2 with jQuery 3.6.0's new nested-focus "protection"
+ * see: https://github.com/select2/select2/issues/5993
+ *
+ * TODO: Recheck with the select2 GH issue and remove once this is fixed on their side
+ */
+function forceFocus() {
+    // find the open dropdown, then find the search field within it
+    const selector = ".select2-container--open .select2-search__field";
+    // use native APIs instead of JQuery
+    document.querySelector<HTMLElement>(selector)?.focus();
+}
+// set the event handler only once globally, NOT within initSelect2(...)
+$(document).on("select2:open", forceFocus);
