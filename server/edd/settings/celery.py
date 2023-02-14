8616ad3e60beccb8fafd04223eaf49c30c6f6a93
William Morrell <WCMorrell@lbl.gov>
2023-02-14 15:10:37 -0800

    Improve formatting of celery settings
    The line length issue is no longer a problem. Moved some things around to put
    like-with-like, adding comments for future work.

--- server/edd/settings/celery.py
+++ server/edd/settings/celery.py
@@ -15,7 +15,11 @@ from .base import env
 CELERY_BROKER_URL = env("BROKER_URL")
 CELERY_BROKER_CONNECTION_RETRY = True
 CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+# Try fixing problems with connection pool to RabbitMQ by disabling pool
+# See: https://github.com/celery/celery/issues/4226
+CELERY_BROKER_POOL_LIMIT = None
 
+# Serialization settings
 CELERY_ACCEPT_CONTENT = {"json", "edd-json"}
 CELERY_TASK_SERIALIZER = "edd-json"
 CELERY_RESULT_SERIALIZER = "edd-json"
@@ -23,58 +27,36 @@ CELERY_TASK_PUBLISH_RETRY = False
 
 # Default queue
 CELERY_TASK_DEFAULT_EXCHANGE = "edd"
-CELERY_TASK_DEFAULT_QUEUE = "edd"
-CELERY_TASK_DEFAULT_ROUTING_KEY = "edd"
+CELERY_TASK_DEFAULT_QUEUE = "default"
+CELERY_TASK_DEFAULT_ROUTING_KEY = "default"
 
 # Queue lists
 # TODO: change "describe" to "setup" when the module name changes
+# TODO: regularize the naming schemes on method names
 CELERY_TASK_QUEUES = (
-    Queue("edd"),
-    Queue("describe.clear_error_report"),
-    Queue("describe.send_describe_failed_email_admin"),
-    Queue("describe.send_describe_failed_email_user"),
-    Queue("describe.send_describe_success_email"),
-    Queue("export.export_table_task"),
-    Queue("export.export_worklist_task"),
-    Queue("load.import_table_task"),
-    Queue("load.send_import_completion_email"),
-    Queue("load.send_import_failure_email_admins"),
-    Queue("load.send_import_failure_email"),
-    Queue("load.send_wizard_failed_email"),
-    Queue("load.send_wizard_paused_email"),
-    Queue("load.send_wizard_success_email"),
-    Queue("load.wizard_execute_loading"),
-    Queue("load.wizard_parse_and_resolve"),
-    Queue("main.link_ice_entry_to_study"),
-    Queue("main.template_sync_species"),
-    Queue("main.unlink_ice_entry_from_study"),
+    Queue("default"),
+    Queue("edd.describe.tasks.clear_error_report"),
+    Queue("edd.describe.tasks.send_describe_failed_email_admin"),
+    Queue("edd.describe.tasks.send_describe_failed_email_user"),
+    Queue("edd.describe.tasks.send_describe_success_email"),
+    Queue("edd.export.tasks.export_table_task"),
+    Queue("edd.export.tasks.export_worklist_task"),
+    Queue("edd.load.tasks.import_table_task"),
+    Queue("edd.load.tasks.send_import_completion_email"),
+    Queue("edd.load.tasks.send_import_failure_email_admins"),
+    Queue("edd.load.tasks.send_import_failure_email"),
+    Queue("edd.load.tasks.send_wizard_failed_email"),
+    Queue("edd.load.tasks.send_wizard_paused_email"),
+    Queue("edd.load.tasks.send_wizard_success_email"),
+    Queue("edd.load.tasks.wizard_execute_loading"),
+    Queue("edd.load.tasks.wizard_parse_and_resolve"),
+    Queue("main.models.measurement_type.lookup_protein_in_uniprot"),
+    Queue("main.models.measurement_type.metabolite_load_pubchem"),
+    Queue("main.tasks.link_ice_entry_to_study"),
+    Queue("main.tasks.template_sync_species"),
+    Queue("main.tasks.unlink_ice_entry_from_study"),
 )
-
-CELERY_TASK_ROUTES = {
-    "edd.describe.tasks.clear_error_report": "describe.clear_error_report",
-    "edd.describe.tasks.send_describe_failed_email_admin": "describe.send_describe_failed_email_admin",
-    "edd.describe.tasks.send_describe_failed_email_user": "describe.send_describe_failed_email_user",
-    "edd.describe.tasks.send_describe_success_email": "describe.send_describe_success_email",
-    "edd.export.tasks.export_table_task": "export.export_table_task",
-    "edd.export.tasks.export_worklist_task": "export.export_worklist_task",
-    "edd.load.tasks.import_table_task": "load.import_table_task",
-    "edd.load.tasks.send_import_completion_email": "load.send_import_completion_email",
-    "edd.load.tasks.send_import_failure_email_admins": "load.send_import_failure_email_admins",
-    "edd.load.tasks.send_import_failure_email": "load.send_import_failure_email",
-    "edd.load.tasks.send_wizard_failed_email": "load.send_wizard_failed_email",
-    "edd.load.tasks.send_wizard_paused_email": "load.send_wizard_paused_email",
-    "edd.load.tasks.send_wizard_success_email": "load.send_wizard_success_email",
-    "edd.load.tasks.wizard_execute_loading": "load.wizard_execute_loading",
-    "edd.load.tasks.wizard_parse_and_resolve": "load.wizard_parse_and_resolve",
-    "main.models.measurement_type.lookup_protein_in_uniprot": "models.measurement_type.lookup_protein_in_uniprot",
-    "main.models.measurement_type.metabolite_load_pubchem": "models.measurement_type.metabolite_load_pubchem",
-    "main.tasks.link_ice_entry_to_study": "tasks.link_ice_entry_to_study",
-    "main.tasks.template_sync_species": "tasks.template_sync_species",
-    "main.tasks.unlink_ice_entry_from_study": "tasks.unlink_ice_entry_from_study",
-}
-
-# Try fixing problems with connection pool to RabbitMQ by disabling pool
-# See: https://github.com/celery/celery/issues/4226
-CELERY_BROKER_POOL_LIMIT = None
+# map task to own queue, leaving out the first (default) queue
+CELERY_TASK_ROUTES = {q.name: q.name for q in CELERY_TASK_QUEUES[1:]}
 
 CELERY_RESULT_BACKEND = env("CELERY_RESULT_BACKEND")
