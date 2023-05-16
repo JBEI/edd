0c2872b988320feeaa3441f97491d91bbdaf7448
William Morrell <WCMorrell@lbl.gov>
2023-05-16 11:42:42 -0700

    Move URL registration to urls.py
    It's a little confusing for some URLs to be registered in the AppConfig
    classes, so instead moving the code into a function in urls.py that gets called
    by AppConfig. Now we can look at urls.py to see everything.

--- server/edd/load/apps.py
+++ server/edd/load/apps.py
@@ -10,17 +10,8 @@ class LoadConfig(AppConfig):
         # The F401 error code is "imported but unused" warning;
         # we ignore it here because we're purposefully importing unused modules
         # to make certain signal handlers are defined at the correct time
-
-        # make sure to load/register all the signal handlers
-        from edd.rest.routers import router, study_router
-
         from . import reporting  # noqa: F401
         from . import signals  # noqa: F401
+        from . import urls
 
-        # plug into the REST API
-        from .rest import views
-
-        study_router.register(r"load", views.LoadRequestViewSet, basename="study_load")
-        router.register(
-            r"load_categories", views.CategoriesViewSet, basename="load_categories"
-        )
+        urls.register_rest_api_calls()
--- server/edd/load/urls.py
+++ server/edd/load/urls.py
@@ -10,3 +10,21 @@ urlpatterns = [
     path("", login_required(views.ImportTableView.as_view()), name="table"),
     path("wizard/", login_required(views.ImportView.as_view()), name="wizard"),
 ]
+
+
+def register_rest_api_calls():
+    # plug into the REST API
+    from edd.rest.routers import router, study_router
+
+    from .rest import views as rest_views
+
+    study_router.register(
+        r"load",
+        rest_views.LoadRequestViewSet,
+        basename="study_load",
+    )
+    router.register(
+        r"load_categories",
+        rest_views.CategoriesViewSet,
+        basename="load_categories",
+    )
