c841919896c4f8d9efb4cbd8b37a73ce4fadb69e
William Morrell <WCMorrell@lbl.gov>
2023-09-29 21:42:34 -0700

    Add logging to failure loading custom favicon
    Some of our deployments are using the default fallback favicon, instead of the
    custom favicon set for each. There's no logging on failure to load the branding
    or favicon, so it's hard to tell why it's failing. Adding the logging should
    help us track down the problem.

--- server/edd/branding/views.py
+++ server/edd/branding/views.py
@@ -1,6 +1,10 @@
+import logging
+
 from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.http import HttpResponse
+
+logger = logging.getLogger(__name__)
 
 
 def favicon(request):
@@ -8,7 +12,8 @@ def favicon(request):
         site = get_current_site(request)
         # must explicitly open ImageField objects
         favicon = site.page.branding.favicon_file.open()
-    except Exception:
+    except Exception as e:
+        logger.exception(f"Failed getting branding favicon, using default: {e}")
         favicon = staticfiles_storage.open("favicon.ico")
     with favicon:
         return HttpResponse(favicon.read(), content_type="image/x-icon")
