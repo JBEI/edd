5ce5ab788fbeb18e02c4d7224d4926e026b3d142
William Morrell <WCMorrell@lbl.gov>
2023-10-17 12:12:42 -0700

    Move threadlocal conftest fixture to root
    An auto-use fixture used to clear out anything in the `request` threadlocal
    variable is moved to the project Python root, instead of in the `main` module.
    When the fixture is in the `main` module, it only applies to tests in that
    module, while EDD has tests in multiple modules that could manipulate the
    `request` threadlocal value.
    
    This helps make tests more isolated and consistent in modules outside of `main`.

--- server/main/tests/conftest.py
+++ server/main/tests/conftest.py
@@ -1,21 +1,6 @@
 import environ
 from django_redis import get_redis_connection
 from pytest import fixture
-from threadlocals import threadlocals
-
-
-@fixture(autouse=True)
-def clear_threadlocal_request():
-    """
-    Resets the request threadlocal variable before and after a test. EDD uses
-    a threadlocal variable to make the current request available everywhere,
-    primarily so the Update model can ensure only one entry is created per
-    request. When running tests that can trigger inserting a new Update to the
-    database, not clearing this causes transaction errors.
-    """
-    threadlocals.set_thread_variable("request", None)
-    yield
-    threadlocals.set_thread_variable("request", None)
 
 
 @fixture(autouse=True, scope="session")
