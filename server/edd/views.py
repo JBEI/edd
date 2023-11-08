ea8258630e1a94bb409a914e238448709bb20538
William Morrell <WCMorrell@lbl.gov>
2023-11-08 15:04:11 -0800

    Fix legacy strain view study lookup
    We need to use a QuerySet `values_list` call to get a listing of all the Study
    IDs, and not a `values` call.

--- server/edd/views.py
+++ server/edd/views.py
@@ -6,7 +6,7 @@ from main.models import Line, Study
 def no_strain_url(request, strain_id):
     """Simplest possible view for the legacy_issue_no_strain_url page."""
     lines = Line.objects.filter(strain_ids=[strain_id])
-    study_ids = lines.values("study_id").distinct()
+    study_ids = lines.values_list("study_id", flat=True).distinct()
     studies = Study.objects.in_bulk(study_ids)
     return TemplateResponse(
         request,
