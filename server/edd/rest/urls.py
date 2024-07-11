62c7b160a20725d6a7608e5225cb32b0a0532c6d
William Morrell <WCMorrell@lbl.gov>
2024-07-10 22:04:49 -0700

    Remove DRF nested routers
    The commit b7d62f54 removed the REST API nested routes for studies because of
    some warnings elsewhere in the code. These never got restored, and there is
    currently no use-case for a nested router. Users should make use of the
    `in_study` parameter for many of the endpoints instead, which is more flexible
    than the nested router approach.
    
    Removing the `drf-nested-routers` package from the installed packages, and
    deleting the now dead code defining a nested router entrypoint. Since there
    isn't much left in `routers.py`, incorporating that code into `urls.py` as
    recommended in the DRF documentation.

--- server/edd/rest/urls.py
+++ server/edd/rest/urls.py
@@ -1,17 +1,26 @@
 from django.urls import include, path
+from rest_framework import routers
 from rest_framework_simplejwt import views as jwt
 
-from . import routers, views
+from . import views
 
 app_name = "edd.rest"
+router = routers.DefaultRouter()
+router.register("assays", views.AssaysViewSet, basename="assays")
+router.register("compartments", views.CompartmentViewSet, basename="compartments")
+router.register("export", views.ExportViewSet, basename="export")
+router.register("lines", views.LinesViewSet, basename="lines")
+router.register("measurements", views.MeasurementsViewSet, basename="measurements")
+router.register("metadata_types", views.MetadataTypeViewSet, basename="metadata_types")
+router.register("protocols", views.ProtocolViewSet, basename="protocols")
+router.register("stream-export", views.StreamingExportViewSet, basename="stream-export")
+router.register("studies", views.StudiesViewSet, basename="studies")
+router.register("types", views.MeasurementTypesViewSet, basename="types")
+router.register("units", views.MeasurementUnitViewSet, basename="units")
+router.register("users", views.UsersViewSet, basename="users")
 
-
-###################################################################################################
-# Use routers & supporting frameworks to construct URL patterns
-###################################################################################################
 urlpatterns = [
-    path("", include(routers.router.urls)),
-    path("", include(routers.study_router.urls)),
+    path("", include(router.urls)),
     path("docs/", views.schema_view.with_ui("swagger", cache_timeout=0), name="docs"),
     path(
         "redoc/",
